f8972bacd7f021f6089a613151ccb87b5ab54724
javinc <javinczki02@gmail.com>
2015-11-20 15:16:16 +0800

    update index

--- index/controller.go
+++ index/controller.go
@@ -2,23 +2,26 @@ package index
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 )
 
 // Handler just to catch / uri
 func Handler(w http.ResponseWriter, r *http.Request) {
-	render := r.Method + " Puto!\n"
-	render += "URL is " + r.URL.String() + "\n"
+	log.Println("index handler")
+
+	render := "Method: " + r.Method + "\n\n"
+	render += "URL: " + r.URL.String() + "\n"
 
 	getParam := r.URL.Query()
 	// get GET params
-	render += "\nGET params: \n"
+	render += "\nGET: \n"
 	for key, value := range getParam {
 		render += "\t" + key + ":" + value[0] + "\n"
 	}
 
-	render += "\nPOST params: \n"
+	render += "\nPOST: \n"
 	// get POST params
 	body, _ := ioutil.ReadAll(r.Body)
 	postParam, _ := url.ParseQuery(string(body))
