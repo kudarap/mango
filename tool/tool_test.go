36a276701d84acb2cfe816e3a3066aeb3d86907a
javinc <javinczki02@gmail.com>
2017-03-18 18:19:16 +0800

    testing test

--- tool/tool_test.go
+++ tool/tool_test.go
@@ -1,1 +1,16 @@
 package tool
+
+import "testing"
+
+func TestInArray(t *testing.T) {
+	n := 1
+	s := []int{0, 1, 2, 3}
+	i, ok := InArray(n, s)
+	if !ok {
+		t.Errorf("failed to to ok")
+	}
+
+	if i != 1 {
+		t.Errorf("failed to to get index")
+	}
+}
