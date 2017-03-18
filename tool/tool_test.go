9c0b2908ce697bd0e2a71824439c792faff89eb2
javinc <javinczki02@gmail.com>
2017-03-18 18:39:23 +0800

    playing with testing

--- tool/tool_test.go
+++ tool/tool_test.go
@@ -3,14 +3,19 @@ package tool
 import "testing"
 
 func TestInArray(t *testing.T) {
-	n := 1
-	s := []int{0, 1, 2, 3}
-	i, ok := InArray(n, s)
+	i, ok := InArray(1, []int{0, 1, 2, 3})
 	if !ok {
-		t.Errorf("failed to to ok")
+		t.Errorf("failed to find needle")
+	} else if i != 1 {
+		t.Errorf("failed to to get needle index")
 	}
+}
 
-	if i != 1 {
-		t.Errorf("failed to to get index")
+func TestNotInArray(t *testing.T) {
+	i, ok := InArray(4, []int{0, 1, 2, 3})
+	if ok {
+		t.Errorf("it should not be found")
+	} else if i != -1 {
+		t.Errorf("it should not have index")
 	}
 }
