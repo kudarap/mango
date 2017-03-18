d5099b7afc2b7b8fad46ae800a67873210da3d15
javinc <javinczki02@gmail.com>
2017-03-19 00:02:45 +0800

    remove old test suites

--- main.go
+++ main.go
@@ -2,27 +2,10 @@ package main
 
 import (
 	"log"
-
-	"github.com/gin-gonic/gin"
-
-	"github.com/javinc/mango/config"
-	foo "github.com/javinc/mango/foo/controller"
-	"github.com/javinc/mango/rest"
 )
 
 func main() {
 	log.Println("[main]", "starting...")
 
-	c := config.Get()
-	r := rest.Router()
-
-	// routing
-	route(r)
-
-	log.Println("[main]", "listening and serving on", c.Port)
-	r.Run(c.Port)
-}
-
-func route(r *gin.Engine) {
-	r.Any("/", foo.Handler)
+	log.Println("[main]", "listening and serving on", 8080)
 }
